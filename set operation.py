a4aaa2d35f0205530336d3f7da00e74e45bc10d8
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-28 16:54:44 +0600

    first commit

---
+++ set operation.py
@@ -0,0 +1,17 @@
+print("\033c")
+mySet = {1, 2, 3, 3, 4, 5, 5, 5}
+print(mySet)
+
+
+mySet.add("Tabassum")
+mySet.add("Kashfia")
+mySet.add("Junaira")
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+li = [1, 2, 3, 2]
+setList = set(li)
+print(setList)
